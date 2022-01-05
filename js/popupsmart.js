78c6bdc1287f8cb007ac8554f32c69b30884d9b7
Marton Csikos <marton.csikos@gmail.com>
2022-01-05 18:52:53 +0100

    Deployed 6f08687 with MkDocs version: 1.2.3

--- js/popupsmart.js
+++ js/popupsmart.js
@@ -4,8 +4,8 @@ script.async = true;
 document.head.appendChild(script);
 
 document.addEventListener('keydown', (e) => {
-  console.log(e.target);
-  e.stopPropagation();
+  const id = e.target.id;
+  if (id && id.slice(0,11) = 'popup-smart') e.stopPropagation();
 });
 
 
