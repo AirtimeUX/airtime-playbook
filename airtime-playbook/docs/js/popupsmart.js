4af5f34c5d3148e51e9effed0ef8cc8a590e0235
Marton Csikos <marton.csikos@gmail.com>
2022-01-05 18:54:23 +0100

    filter event propagation to only work when popupsmart popup is active, so keyboard nav can be used in the rest of the app

--- airtime-playbook/docs/js/popupsmart.js
+++ airtime-playbook/docs/js/popupsmart.js
@@ -4,7 +4,8 @@ script.async = true;
 document.head.appendChild(script);
 
 document.addEventListener('keydown', (e) => {
-  e.stopPropagation();
+  const id = e.target.id;
+  if (id && id.slice(0,11) === 'popup-smart') e.stopPropagation();
 });
 
 
