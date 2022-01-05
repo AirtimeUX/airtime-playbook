0c1733d0eef4dff04fced4c04a0edc0972b1da4c
Marton Csikos <marton.csikos@gmail.com>
2022-01-05 17:25:41 +0100

    Deployed 5157196 with MkDocs version: 1.2.3

--- js/popupsmart.js
+++ js/popupsmart.js
@@ -1,8 +1,6 @@
+var script = document.createElement("script");
+script.src = "https://apiv2.popupsmart.com/api/Bundle/377681"
+script.async = true;
+document.head.appendChild(script);
 
-document.addEventListener("DOMContentLoaded", function() {
-  var script = document.createElement("script");
-  script.src = "https://apiv2.popupsmart.com/api/Bundle/377681"
-  script.async = true;
-  document.head.appendChild(script);
-});
 
